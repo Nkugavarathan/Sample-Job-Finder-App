d4033bd0cb77431437993787aa86a7238192d061
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-20 19:10:56 +0530

    navbar styled and linked

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react"
+import Navbar from "./components/Navbar"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import { Routes, Route } from "react-router-dom"
+import Contact from "./Pages/Contact"
+import Products from "./Pages/Products"
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="products" element={<Products />} />
+        </Routes>
+      </div>
+    </div>
+  )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from "react"
-
+import { Link } from "react-router-dom"
 export default function Navbar() {
   return (
-    <div>
+    <div className="navbar">
       <img src="" alt="" />
       <div className="navlist">
-        <a href="">Home</a>
-        <a href="">Products</a>
-        <a href="">About</a>
-        <a href="">Contact</a>
+        <Link to="/">
+          <a href="">Home</a>{" "}
+        </Link>
+        <Link to="products">
+          <a href="">Products</a>
+        </Link>
+        <Link to="about">
+          <a href="about">About</a>
+        </Link>
+        <Link to="contact">
+          <a href="contact">Contact</a>
+        </Link>
       </div>
       <button>Get Started</button>
     </div>
