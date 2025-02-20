7cf0bd0102a78980689063c097a41ae470ba9b89
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-20 17:49:34 +0530

    Navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div className="navlist">
+        <a href="">Home</a>
+        <a href="">Products</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+      </div>
+      <button>Get Started</button>
+    </div>
+  )
+}
